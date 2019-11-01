60452e153d62c96f78ea3ad1853ef64533370040
Josue Cespedes <josuecss@yahoo.com>
2019-11-01 17:51:13 -0400

    final

--- prueba.js
+++
@@ -1,7 +0,0 @@
-var Session = {
-    cookie:
-     { path: '/',
-       _expires: 2019-11-01T12:07:38.575Z,
-       originalMaxAge: 36000000,
-       httpOnly: true },
-    user: 'josue@josue.com' }
